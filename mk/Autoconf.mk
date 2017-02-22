db370e47ce045dafd354a1a1109b49ec60478bce
Gerd Hoffmann <kraxel@redhat.com>
2017-02-22 10:00:10 +0100

    update maintainer targets

--- mk/Autoconf.mk
+++ mk/Autoconf.mk
@@ -162,7 +162,7 @@ config: Make.config
 	@true
 
 Make.config: $(srcdir)/GNUmakefile
-	@printf "$(make-config-q)" > $@
+	@echo -e "$(make-config-q)" > $@
 	@echo
 	@echo "Make.config written, edit if needed"
 	@echo
--- mk/Maintainer.mk
+++ mk/Maintainer.mk
@@ -12,10 +12,19 @@ sync:: distclean
 	chmod 444 $(srcdir)/INSTALL $(srcdir)/mk/*.mk
 
 
-repository := $(shell basename $(PWD))
+repository  := $(shell basename $(PWD))
+usetito     := $(shell if test -d .tito; then echo yes; else echo no; fi)
 release-dir ?= $(HOME)/projects/Releases
 release-pub ?= bigendian.kraxel.org:/public/vhosts/www.kraxel.org/releases/$(repository)
-tarball = $(release-dir)/$(repository)-$(VERSION).tar
+tarball      = $(release-dir)/$(repository)-$(VERSION).tar
+
+ifeq ($(usetito),yes)
+
+.PHONY: release
+release:
+	@echo "tito config found, so use this ;)"
+
+else
 
 $(tarball).gz:
 	git tag -m "release $(VERSION)" "$(VERSION)"
@@ -24,7 +33,11 @@ $(tarball).gz:
 		-o $(tarball) $(VERSION)
 	gzip $(tarball)
 
+$(tarball).gz.asc: $(tarball).gz
+	gpg --detach-sign --armor $(tarball).gz
+
 .PHONY: release
 release: $(tarball).gz
-	gpg --detach-sign --armor $(tarball).gz
 	scp $(tarball).gz* $(release-pub)
+
+endif
